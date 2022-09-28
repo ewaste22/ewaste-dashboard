0c03f99297e54a181d79cb85da65dc673252deb2
didapatria <didapatria3@gmail.com>
2022-09-28 22:38:33 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pages/auth/Login';
+import Dashboard from './pages/dashboard/Dashboard';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Dashboard />} /> */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
