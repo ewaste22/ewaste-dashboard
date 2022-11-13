ca0201d383c283ebae084734caf396d63b518c44
SahidJafar <sahid11.sj@gmail.com>
2022-11-13 12:49:50 +0700

    Menambahkan component footer

---
+++ src/components/dashboard/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer(){
+    return(
+        <>
+        <div className="bg-[#85C681] absolute bottom-0 w-screen h-12 text-white flex justify-center items-center">
+        <p>© E-Waste 2022</p>
+      </div>
+</>
+    );
+}
