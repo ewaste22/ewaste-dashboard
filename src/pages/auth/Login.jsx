df7cd23a9e15d2a433cb8dcc46a666b3379e2121
SahidJafar <sahid11.sj@gmail.com>
2023-01-09 13:03:28 +0700

    Fungsi Login Admin User

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,4 +1,4 @@
-import {React, useState, useEffect} from 'react';
+import { React, useState, useEffect } from 'react';
 import axios from 'axios';
 import { ToastContainer, toast } from "react-toastify";
 import swal from "sweetalert";
@@ -19,31 +19,17 @@ export default function Login() {
 
   const login = () => {
     axios.post("http://localhost:8000/api/v1/auth/admin/login", {
-      email_admin: String(email_admin.target.value),
-      password: String(password.target.value),
-    })
-    .then((response) => {
-      localStorage.setItem("token", response.data.data.token);
-      setTimeout(() => {
-        window.location.reload();
-      }, 1000);
-      swal("Anda berhasil masuk!", {
-        icon: "success",
+      email_admin: email_admin,
+      password: password
+    }).then((response) => {
+        localStorage.setItem("token", response.data.token);
+        setTimeout(() => {
+          window.location.reload();
+        }, 1000);
+      })
+      .catch((error) => {
+        toast(error.response.data.message);
       });
-    })
-    .catch((error) => {
-      if (Array.isArray(error.response.data.message)) {
-        error.response.data.message.forEach((err) => {
-          toast(err, {
-            type: "error",
-          });
-        });
-      } else {
-        toast("email or password are wrong", {
-          type: "error",
-        });
-      }
-    });
   }
   // const [msg, setMsg] = useState('');
   // const navigate = useNavigate();
@@ -80,21 +66,19 @@ export default function Login() {
           <h1 className="text-[#2D5030] text-[40px] leading-[60px]">E-Waste</h1>
         </div>
         <div>
-        <h1 className="font-bold text-4xl text-black mb-2">Log In</h1>
-        <p>Welcome!</p>
-        <ToastContainer />
+          <h1 className="font-bold text-4xl text-black mb-2">Log In</h1>
+          <p>Welcome!</p>
+          <ToastContainer />
         </div>
         {!isLoggedIn ? (
-           <form>
-           <div className="flex flex-col gap-6 mt-10">
-             <input type="text" className="px-3 py-2 w-full bg-gray-300 outline-none rounded-md" placeholder="Username" onChange={setEmail} />
-             <input type="password" className="px-3 py-2 w-full bg-gray-300 outline-none rounded-md" placeholder="Password" onChange={setPassword} />
-             <button onClick={login} className="btn rounded-3xl bg-[#85C681] border-none text-white text-lg shadow-xl hover:bg-[#85C681] drop-shadow-xl">
-               Login
-             </button>
-           </div>
-         </form>
-         ) : null } 
+          <div className="flex flex-col gap-6 mt-10">
+            <input type="text" className="px-3 py-2 w-full bg-gray-300 outline-none rounded-md" placeholder="Username" onChange={(e) => setEmail(e.target.value)} />
+            <input type="password" className="px-3 py-2 w-full bg-gray-300 outline-none rounded-md" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+            <button type='button' onClick={login} className="btn rounded-3xl bg-[#85C681] border-none text-white text-lg shadow-xl hover:bg-[#85C681] drop-shadow-xl">
+              Login
+            </button>
+          </div>
+        ) : null}
       </div>
     </div>
   );
