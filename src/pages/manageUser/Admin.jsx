5308f6a3c3b4727ca129d8b8685e552e8ae4fb68
SahidJafar <sahid11.sj@gmail.com>
2023-01-19 16:22:14 +0700

    Get All Data Management User clear

--- src/pages/manageUser/Admin.jsx
+++ src/pages/manageUser/Admin.jsx
@@ -1,10 +1,26 @@
 import HeadBar from '../../components/dashboard/HeadBar';
 import SideBar from '../../components/dashboard/SideBar';
 import { Link } from "react-router-dom";
-import { AiOutlineBell } from 'react-icons/ai';
-import { IoMdExit } from "react-icons/io";
+import { useState, useEffect } from 'react';
+import axios from "axios";
 
 export default function Admin() {
+  const [admin, setAdmin] = useState([]);
+
+  const getAdmin = async () => {
+    await axios.get('http://localhost:8000/api/v1/admin/admin/', {
+      headers:{
+        Authorization: "Bearer " + localStorage.getItem("token"),
+      }
+    }).then(res => {
+      console.log(res.data.data.admin)
+      setAdmin(res.data.data.admin)
+    }).catch(err => console.log(err))
+  }
+  
+  useEffect(() => {
+      getAdmin()
+  }, [])
     return(   
     <>
       <SideBar />
@@ -38,7 +54,7 @@ export default function Admin() {
                     No
                   </th>
                   <th className='px-5 py-3 border-b-2 text-center  text-xs font-semibold text-gray-600 uppercase tracking-wide'>
-                    ID
+                    Admin ID
                   </th>
                   <th className=' px-5 py-3 border-b-2  text-center text-xs font-semibold text-gray-600 uppercase tracking-wide'>
                     Name
@@ -53,18 +69,22 @@ export default function Admin() {
               </thead>
 
             <tbody>
+            {
+                (admin.length > 0) ? (
+                  admin.map((item, index) => {
+                return(
               <tr>
                 <td className='px-5 py-5 text-sm text-center'>
-                    1
+                {index + 1}
                 </td>
                 <td className='px-5 py-5 text-sm text-center'>
-                  <p>00001</p>
+                  <p>{item.id}</p>
                 </td>
                 <td className='px-5 py-5 text-sm text-center'>
-                  <p>Aldi Ageng</p>
+                  <p>{item.fullname_admin}</p>
                 </td>
                 <td className='px-5 py-5 text-sm text-center'>
-                  <p>aldiageng@gmail.com</p>
+                  <p>{item.email_admin}</p>
                 </td>
                 <td class="py-3 px-6 text-center">
                                     <div class="flex item-center justify-center">
@@ -87,42 +107,14 @@ export default function Admin() {
                                     </div>
                                 </td>
                          </tr>
-
-              <tr>
-                <td className='px-5 py-5 text-sm text-center'>
-                    2
-                </td>
-                <td className='px-5 py-5 text-sm text-center'>
-                  <p>00002</p>
-                </td>
-                <td className='px-5 py-5 text-sm text-center'>
-                  <p>Aji Nuansa</p>
-                </td>
-                <td className='px-5 py-5 text-sm text-center'>
-                  <p>ajinuansa@gmail.com</p>
-                </td>
-                <td class="px-5 py-5 text-sm">
-                                    <div class="flex item-center justify-center">
-                                        <div class="w-4 mr-2 transform hover:text-gray-800 hover:scale-110">
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
-                                            </svg>
-                                        </div>
-                                        <div class="w-4 mr-2 transform hover:text-gray-800 hover:scale-110">
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
-                                            </svg>
-                                        </div>
-                                        <div class="w-4 mr-2 transform hover:text-gray-800 hover:scale-110">
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                                            </svg>
-                                        </div>
-                                    </div>
-                                </td>
-              </tr>
-            </tbody>
+                             )
+                            })
+                          ):(
+                         <tr>
+                            <td colSpan="4" className="text-center">No data</td>
+                        </tr>
+                        )}
+                 </tbody>
             </table>
           </div>
           {/* <div className='px-5 py-5 bg-white border-t flex flex-col xs:flex-row items-center xs:justify-between '>
